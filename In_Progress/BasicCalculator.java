0005e8b2f8d7c6eb779b61a797739ca7ee800c7a
ChrIs8279 <159035346+ChrIs8279@users.noreply.github.com>
2025-02-26 14:31:31 -0400

    in progress or started projects.

---
+++ In_Progress/BasicCalculator.java
@@ -0,0 +1,35 @@
+public class BasicCalculator {
+	public static void main(String[] args) {
+		while (true) {
+			System.out.println("enter first digit, then click enter. then second digit: ");
+				string numone = System.console().readLine();
+				int x = Integer.parseInt(numone);
+				string numtwo = System.console().readLine();
+				int y = Integer.parseInt(numtwo);
+				System.out.println("you have selected : "x "and "y );
+				System.out.println("whaat would you like to do with these numbers?");
+				system.out.println("x = multiplication");
+				system.out.println("/ = division");
+				system.out.println("+ = addition");
+				system.out.println("- = subtraction");
+				string opperator = System.console().readLine();
+				if (opperator = x){
+					System.out.println(x "x" y "=" x*y);
+				} else {
+					if (opperator = /){
+						System.out.println(x "x" y "=" x/y);
+					}else{
+						if (opperator = + );
+						System.out.println(x "+" y "=" x+y);
+					}else{
+						system.out.println(x "-" y "=" x-y);
+					}
+				}
+		}
+	}
+}
+					
+				
+				
+				
+		
---
+++ In_Progress/copypaste.py
@@ -0,0 +1,9 @@
+# with open("input.txt", "r") as f:
+#     file_read = f.read()
+#     print(file_read)
+
+
+with open("inpot.txt", 'r') as rf:
+    with open("output.txt", 'w') as wf:
+        textlist = rf.read
+        print(textlist)
